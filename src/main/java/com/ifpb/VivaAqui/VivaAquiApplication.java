2bc89cc132143884dfd4851b78f7efbe1a9b638d
Iasmim Siqueira <iasmimsas@gmail.com>
2024-07-09 15:36:06 -0300

    first commit

---
+++ src/main/java/com/ifpb/VivaAqui/VivaAquiApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.VivaAqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaAquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpb/VivaAqui/VivaAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.VivaAqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaAquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
